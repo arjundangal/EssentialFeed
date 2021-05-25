febb85ea76a82c167a59c5c8fd7f6c0f28aa0ef3
Arjun Dangal <arjundangal4@gmail.com>
2021-05-25 11:47:24 +0545

    inject URL

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -9,20 +9,22 @@ import XCTest
 
 class RemoteFeedLoader{
     let client : HTTPClient
-    init(client : HTTPClient){
+    let url : URL
+    
+    init(url : URL, client : HTTPClient){
         self.client = client
+        self.url = url
     }
     
     func load(){
-        client.get(from:  URL(string: "https://aurl.com"))
+        client.get(from:  url )
         
     }
 }
 
 protocol HTTPClient {
      func get(from url: URL?)
-    
-}
+ }
 
 class HTTPClientSpy : HTTPClient{
     var requestedURL : URL?
@@ -36,16 +38,18 @@ class HTTPClientSpy : HTTPClient{
 class RemoteFeedLoaderTests : XCTestCase{
     
     func test_init_doesNotRequestDataFromURL(){
+        let url =  URL(string: "https://aurl.com")!
         let client = HTTPClientSpy()
-         _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL(){
+        let url =  URL(string: "https://aurl.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url, client: client)
         sut.load()
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(url, client.requestedURL)
     }
     
 }
