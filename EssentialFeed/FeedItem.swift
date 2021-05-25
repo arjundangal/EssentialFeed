25cd24808bd9c7d90c7226ef4c5849f8247a718d
Arjun Dangal <arjundangal4@gmail.com>
2021-05-25 11:14:54 +0545

    create feed item

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Arjun Dangal on 25/05/2021.
+//
+
+import Foundation
+
+struct FeedItem  {
+    let id : UUID
+    let description : String?
+    let location : String?
+    let imageURL : URL
+    
+}
