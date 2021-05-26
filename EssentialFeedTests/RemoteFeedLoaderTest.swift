eb1f977dea56e8f4ab64f3a96e3dc38db7ba4d0f
Arjun Dangal <arjundangal4@gmail.com>
2021-05-26 13:46:07 +0545

    add test and feed item conform to equatable

--- EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -63,6 +63,10 @@ class RemoteFeedLoaderTests : XCTestCase{
          }
          }
     
+//    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList(){
+//        let (sut, client) = makeSUT()
+//
+//    }
   
     
     
@@ -70,14 +74,14 @@ class RemoteFeedLoaderTests : XCTestCase{
     //MARK:- Helpers
     
     private func expect(_ sut : RemoteFeedLoader,  toCompleteWithError error : RemoteFeedLoader.Error, when action : () -> Void, file : StaticString =  #filePath, line : UInt = #line){
-        var capturedErrors = [RemoteFeedLoader.Error]()
+        var capturedErrors = [RemoteFeedLoader.Result]()
         sut.load{
             capturedErrors.append($0)
         }
         
         action()
         
-        XCTAssertEqual(capturedErrors,[error],file: file, line: line)
+        XCTAssertEqual(capturedErrors,[.failure(error)],file: file, line: line)
 
     }
     
