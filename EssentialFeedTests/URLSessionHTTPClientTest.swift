63dd16465cbb4615aa8f1e5216070800382b0235
Arjun Dangal <arjundangal4@gmail.com>
2021-06-01 10:32:40 +0545

    factory method for creating SUT

--- EssentialFeedTests/URLSessionHTTPClientTest.swift
+++ EssentialFeedTests/URLSessionHTTPClientTest.swift
@@ -54,7 +54,7 @@ class URLSessionHTTPClientTest : XCTestCase{
         }
         
         
-        URLSessionHTTPCLient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         wait(for: [exp], timeout: 1.0)
 
     }
@@ -65,11 +65,10 @@ class URLSessionHTTPClientTest : XCTestCase{
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(url : url, data : nil, response : nil, error : error)
         
-        let sut = URLSessionHTTPCLient()
-        
+ 
         let exp = expectation(description: "wait for completion")
         
-        sut.get(from : url){result in
+        makeSUT().get(from : url){result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertNotNil(receivedError)
@@ -84,6 +83,11 @@ class URLSessionHTTPClientTest : XCTestCase{
      }
 
     //MARK:- Helpers
+    
+    private func makeSUT() -> HTTPClient {
+        return URLSessionHTTPCLient()
+    }
+    
     private class URLProtocolStub : URLProtocol{
         private static var stub : Stub?
         private static var requestObserver : ((URLRequest) -> Void)?
