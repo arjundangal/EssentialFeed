1aa37375c677cd65b962edce0fcc8e0a3e5dcaf9
Arjun Dangal <arjundangal4@gmail.com>
2021-06-18 10:18:10 +0545

    Save Feed Request Cache Deletion

--- EssentialFeedTests/Feed Cache/CachedFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CachedFeedUseCaseTests.swift
@@ -6,17 +6,29 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    var cacheDeletionCount = 0
+    
+    func deleteCachedFeed(_ items : [FeedItem]){
+        cacheDeletionCount = cacheDeletionCount + 1
+
+     }
+    
 }
 
 class LocalFeedLoader {
-    
+    let store : FeedStore
     init(store : FeedStore) {
-        
-        
+        self.store = store
     }
+    
+    func save(_ items : [FeedItem]){
+        store.deleteCachedFeed(items)
+    }
+    
 }
 
 class CachedFeedUseCaseTests: XCTestCase {
@@ -26,7 +38,26 @@ class CachedFeedUseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store : store)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
+    
+    func test_save_requestsCacheDeletion(){
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.cacheDeletionCount, 1)
+    }
+    
+    //MARK:- Helpers
+    
+    private func uniqueItem() -> FeedItem{
+        return FeedItem(id: UUID(), description: nil, location: nil, imageURL: anyURL())
+    }
+    private func anyURL() -> URL{
+        return URL(string: "http://aurl.com")!
 
+    }
 }
 
 
